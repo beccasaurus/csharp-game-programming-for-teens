6a24c4ca8c8f18d77601e9156a75f5336bf089c1
remi <remi@remitaylor.com>
2012-07-21 19:16:00 -0700

    Dart animation main loop

---
+++ canvas/game.dart
@@ -0,0 +1,52 @@
+#import('dart:html');
+#import('dart:math');
+
+void main() {
+  new Game().run();
+}
+
+class Game {
+  Document doc;
+  CanvasRenderingContext2D ctx;
+  HTMLCanvasElement canvas;
+  int width;
+  int height;
+  bool stopped = false;
+  Random random;
+
+  Game() {
+    canvas = document.query('#canvas');
+    ctx = canvas.getContext("2d");
+    width = canvas.width;
+    height = canvas.height;
+    random = new Random();
+
+    window.console.log(this);
+  }
+
+  void run() => start();
+
+  void start() {
+    print("starting game");
+    window.requestAnimationFrame(loop);
+  }
+
+  void loop(int time) {
+    update();
+    draw();
+    window.requestAnimationFrame(loop);
+  }
+
+  update() {
+    ctx.fillStyle = "#FF00FF";
+  }
+
+  draw() {
+    ctx.fillRect(
+      100 + random.nextInt(200),
+      100 + random.nextInt(200),
+      100 + random.nextInt(200),
+      100 + random.nextInt(200)
+    );
+  }
+}
