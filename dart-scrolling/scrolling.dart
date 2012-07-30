245f9504af1df2edb11b8207f75dab85e8f0bd17
remi <remi@remitaylor.com>
2012-07-29 19:38:41 -0700

    1st stab at smooth, sub-tile scrolling

--- dart-scrolling/scrolling.dart
+++ dart-scrolling/scrolling.dart
@@ -1,7 +1,7 @@
 #import('dart:html');
 #import('dart:math');
 
-main() => new CanvasScrolling().run();
+main() => new CanvasScrolling().start();
 
 class CanvasScrolling {
   CanvasRenderingContext2D _ctx;
@@ -17,14 +17,15 @@ class CanvasScrolling {
   DivElement _map;
   ImageElement _palette;
   int _paletteColumns = 5;
+  bool _running;
 
-  List<int> tiles = const [17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 16, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 8, 113, 113, 113, 113, 126, 123, 137, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 136, 123, 123, 123, 134, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 136, 127, 123, 119, 134, 113, 91, 113, 113, 113, 113, 113, 102, 100, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 113, 128, 123, 125, 113, 113, 113, 113, 113, 113, 113, 92, 139, 80, 113, 113, 113, 96, 94, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 102, 122, 100, 113, 113, 113, 96, 94, 113, 113, 7, 113, 113, 135, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 92, 110, 139, 80, 113, 113, 113, 96, 94, 113, 113, 7, 113, 113, 113, 113, 113, 113, 130, 113, 113, 113, 113, 113, 113, 113, 81, 81, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 135, 113, 113, 113, 113, 113, 113, 138, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 126, 40, 134, 113, 113, 113, 113, 113, 113, 130, 113, 113, 106, 104, 113, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 128, 125, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 84, 84, 104, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 86, 115, 113, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7];
+  List<int> tilemap = const [17, 2, 17, 2, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 16, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 8, 113, 113, 113, 113, 126, 123, 137, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 136, 123, 123, 123, 134, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 136, 127, 123, 119, 134, 113, 91, 113, 113, 113, 113, 113, 102, 100, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 113, 128, 123, 125, 113, 113, 113, 113, 113, 113, 113, 92, 139, 80, 113, 113, 113, 96, 94, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 102, 122, 100, 113, 113, 113, 96, 94, 113, 113, 7, 113, 113, 135, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 92, 110, 139, 80, 113, 113, 113, 96, 94, 113, 113, 7, 113, 113, 113, 113, 113, 113, 130, 113, 113, 113, 113, 113, 113, 113, 81, 81, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 135, 113, 113, 113, 113, 113, 113, 138, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 126, 40, 134, 113, 113, 113, 113, 113, 113, 130, 113, 113, 106, 104, 113, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 128, 125, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 84, 84, 104, 113, 113, 7, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113, 86, 115, 113, 113, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7];
 
   CanvasScrolling() {
     _map = document.query('#map');
     _canvas = document.query('#canvas');
     _ctx = _canvas.getContext('2d');
-    _scrollPosition = new Point(100, 10);
+    _scrollPosition = new Point(0, _mapHeight - _canvas.height);
     _pressedKeys = new Set<String>();
     _palette = new ImageElement(src: 'palette.png');
     _palette.on.load.add((e) => _info('loading', 'Loaded palette.png'));
@@ -41,12 +42,19 @@ class CanvasScrolling {
     }
   }
 
-  run() => window.requestAnimationFrame(_loop);
+  start() {
+    _running = true;
+    window.requestAnimationFrame(_loop);
+  }
+
+  stop() => _running = false;
 
   _loop(int tick) {
-    _update(tick);
-    _draw(tick);
-    window.requestAnimationFrame(_loop);
+    if (_running) {
+      _update(tick);
+      _draw(tick);
+      window.requestAnimationFrame(_loop);
+    }
   }
   
   _update(int tick) {
@@ -61,8 +69,8 @@ class CanvasScrolling {
 
     if (_scrollPosition.x < 0) _scrollPosition.x = 0;
     if (_scrollPosition.y < 0) _scrollPosition.y = 0;
-    if (_scrollPosition.x > _mapWidth - _canvas.width) _scrollPosition.x = _mapWidth - _canvas.width;
-    if (_scrollPosition.y > _mapHeight - _canvas.height) _scrollPosition.y = _mapHeight - _canvas.height;
+    if (_scrollPosition.x >= _mapWidth - _canvas.width) _scrollPosition.x = _mapWidth - _canvas.width;
+    if (_scrollPosition.y >= _mapHeight - _canvas.height) _scrollPosition.y = _mapHeight - _canvas.height;
   }
 
   _info(id, value) {
@@ -83,21 +91,43 @@ class CanvasScrolling {
     _info('scrollPosition', 'x: ${_scrollPosition.x}, y: ${_scrollPosition.y}');
     
     // find starting tiles
-    var startingX = _scrollPosition.x / _tileSize;
-    var xPart = (_scrollPosition.x % _tileSize).toInt();
-    var startingY = _scrollPosition.y / _tileSize;
-    var yPart = (_scrollPosition.y % _tileSize).toInt();
-    var tileIndex = ((startingY.floor() * (_mapWidth / _tileSize)) + startingX.floor()).toInt();
-    var tileId = tiles[tileIndex];
-    _info('startingTile', '$tileId  (Down: $startingY [${yPart}px], Across: $startingX [${xPart}px])');
+    int mapTilesAcross = (_mapWidth / _tileSize).toInt();
+    int mapTilesDown = (_mapHeight / _tileSize).toInt();
+    int firstTileIndex = (_scrollPosition.y / _tileSize).toInt() * mapTilesAcross + (_scrollPosition.x / _tileSize).toInt();
+    int partialYpx = _tileSize - (_scrollPosition.y % _tileSize).toInt();
+    int partialXpx = _tileSize - (_scrollPosition.x % _tileSize).toInt();
+    int row = 0;
 
-    // draw a tile
-    var paletteX = ((tileId - 1) % _paletteColumns) * (_tileSize + _tileSpacing);
-    var paletteY = (((tileId - 1) / _paletteColumns).toInt() * (_tileSize + _tileSpacing));
-    _ctx.drawImage(_palette,
-      paletteX + xPart, paletteY + yPart, _tileSize - xPart, _tileSize - yPart,
-      0, 0, _tileSize - xPart, _tileSize - yPart
-    );
-    _info('rendering', 'x: $paletteX, y: $paletteY');
+    _info('startingTile', 'First Index: $firstTileIndex');
+
+    // render all
+    for (int y = 0; y < _canvas.height;) {
+      int tileHeight = (y == 0) ? partialYpx : _tileSize;
+      int tileIndex = (mapTilesAcross * row) + firstTileIndex;
+      if (tileIndex >= tilemap.length) break; // shouldn't need this! XXX
+
+      for (int x = 0; x < _canvas.width;) {
+        if (tileIndex >= tilemap.length) break; // shouldn't need this! XXX
+        int tileWidth = (x == 0) ? partialXpx : _tileSize;
+        num tileId = tilemap[tileIndex];
+        num paletteX = ((tileId - 1) % _paletteColumns) * (_tileSize + _tileSpacing);
+        num paletteY = (((tileId - 1) / _paletteColumns).toInt() * (_tileSize + _tileSpacing));
+        if (tileHeight != _tileSize) paletteY += _tileSize - partialYpx;
+        if (tileWidth != _tileSize) paletteX += _tileSize - partialXpx;
+
+        _info('rendering', '($paletteX,$paletteY) ${tileWidth}x${tileHeight} ---> ($x,$y) ${tileWidth}x${tileHeight}');
+        _ctx.drawImage(
+          _palette,
+          paletteX, paletteY, tileWidth, tileHeight,
+          x, y, tileWidth, tileHeight
+        );
+
+        tileIndex++;
+        x += tileWidth;
+      }
+
+      y += tileHeight;
+      row++;
+    }
   }
 }
